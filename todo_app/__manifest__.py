fe56c4e300e22e6ff3d7a67a28941db5b3e89cc9
Varo1234 <alvaro18gp@gmail.com>
2021-10-04 12:05:13 -0500

    Commit 2

--- todo_app/__manifest__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-{
-    'name': 'To-Do Application',
-    'description': 'Manage your personal To-Do tasks.',
-    'author': 'Alvaro Gutierrez',
-    'depends': ['base'],
-    'application': True,
-}
